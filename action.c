d77fe5bd516a7085fd24ecd939e6b0816e2a9489
gotmor <gotmor@f2baff5b-bf2c-0410-a398-912abdc3d8b2>
2007-06-03 20:05:03 +0000

    fixed bug in scroll
    git-svn-id: http://dzen.googlecode.com/svn/trunk@62 f2baff5b-bf2c-0410-a398-912abdc3d8b2

--- action.c
+++ action.c
@@ -231,8 +231,10 @@ a_togglestick(char * opt[]) {
     return 0;
 }
 
-void
+int
 scroll(int n) {
+    if(dzen.slave_win.last_line_vis < dzen.slave_win.max_lines)
+        return;
     if(dzen.slave_win.first_line_vis + n < 0) {
         dzen.slave_win.first_line_vis = 0;
         dzen.slave_win.last_line_vis = dzen.slave_win.max_lines;
